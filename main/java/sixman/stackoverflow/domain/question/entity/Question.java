10381b710739637d9db063d5db509824f99c4683
mhyjw98 <130345609+mhyjw98@users.noreply.github.com>
2023-08-14 22:09:44 +0900

    feat ✨ 질문글과 태그 CRUD, 질문 추천 기능 구현 #47

---
+++ main/java/sixman/stackoverflow/domain/question/entity/Question.java
@@ -0,0 +1,77 @@
+package sixman.stackoverflow.domain.question.entity;
+
+import lombok.*;
+import sixman.stackoverflow.domain.answer.entitiy.Answer;
+import sixman.stackoverflow.domain.member.entity.Member;
+import sixman.stackoverflow.domain.questionrecommend.entity.QuestionRecommend;
+import sixman.stackoverflow.domain.questiontag.entity.QuestionTag;
+import sixman.stackoverflow.global.entity.BaseEntity;
+import sixman.stackoverflow.global.entity.TypeEnum;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Integer views;
+
+    private int recommendCount;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QuestionRecommend> questionRecommends = new ArrayList<>();
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QuestionTag> questionTags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Answer> answers = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setQuestionTags(List<QuestionTag> questionTags) {
+        this.questionTags.clear();
+        if (questionTags != null) {
+            this.questionTags.addAll(questionTags);
+        }
+    }
+
+    public void applyRecommend(TypeEnum type) {
+        if (type == TypeEnum.UPVOTE) {
+            this.recommendCount++;
+        } else {
+            this.recommendCount--;
+        }
+    }
+
+    public boolean hasRecommendationFrom(Member member) {
+        return this.questionRecommends.stream()
+                .anyMatch(recommend -> recommend.getMember().equals(member));
+    }
+}
